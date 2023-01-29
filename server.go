c2af7819a2a2ee5d4c1ed359a0aa8f739e68400d
ghager93 <ghager93@gmail.com>
2023-01-30 05:56:06 +1100

    Skeleton code

---
+++ server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World!")
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database.")
+	}
+
+	db.AutoMigrate()
+
+	e := echo.New()
+	e.GET("/hello", hello)
+	e.Logger.Fatal(e.Start(":1323"))
+}
