6317f775d28879d066ef889cc887d09925d78c19
ghager93 <ghager93@gmail.com>
2023-03-12 00:43:34 +1100

    Invalid Post tests passing.

--- api/simple_api_test.go
+++ api/simple_api_test.go
@@ -81,3 +81,45 @@ func TestCreateSavesToDB(t *testing.T) {
 	assert.Equal(t, testBody["name"], simple.Name)
 	assert.Equal(t, testBody["number"], simple.Number)
 }
+
+func TestCreateInvalidRequestReturns400(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+
+	testBody := map[string]interface{}{
+		"name": "john",
+	} 
+	testBodyBytes, _ := json.Marshal(testBody)
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/api/simple", bytes.NewBuffer(testBodyBytes))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, Create(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+	}	
+}
+
+func TestCreateInvalidRequestNotSavedToDB(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+
+	testBody := map[string]interface{}{
+		"name": "john",
+	} 
+	testBodyBytes, _ := json.Marshal(testBody)
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/api/simple", bytes.NewBuffer(testBodyBytes))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	assert.NoError(t, Create(c))
+	
+	var simple models.Simple
+	result := db.DbManager().First(&simple)
+	assert.Error(t, result.Error)
+}
