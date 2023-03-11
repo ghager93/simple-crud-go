9451025328c7738390272818bece6a36d5b789f3
ghager93 <ghager93@gmail.com>
2023-03-11 21:39:06 +1100

    Valid Post save

--- api/simple_api_test.go
+++ api/simple_api_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"simple-crud/go/db"
+	"simple-crud/go/models"
 )
 
 func setupTest(t *testing.T) func() {
@@ -60,10 +61,23 @@ func TestCreateSavesToDB(t *testing.T) {
 	teardown := setupTest(t)
 	defer teardown()
 
+	testBody := map[string]interface{}{
+		"name": "john",
+		"number": 1234,
+	} 
+	testBodyBytes, _ := json.Marshal(testBody)
+
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/api/simple", nil)
+	req := httptest.NewRequest(http.MethodPost, "/api/simple", bytes.NewBuffer(testBodyBytes))
+	req.Header.Set("Content-Type", "application/json")
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
-	Create(c)
+	assert.NoError(t, Create(c))
+	
+	var simple models.Simple
+	db.DbManager().First(&simple)
+	
+	assert.Equal(t, testBody["name"], simple.Name)
+	assert.Equal(t, testBody["number"], simple.Number)
 }
